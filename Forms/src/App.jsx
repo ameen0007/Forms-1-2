d84d6a07892d1baea10ecc8ff807b380e417860a
ameen0007 <122903723+ameen0007@users.noreply.github.com>
2023-08-03 15:31:34 +0530

    Add files via upload

---
+++ Forms/src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment, useState } from "react";
+import { Form1 } from "./componets/Form1";
+import { Form2 } from "./componets/Form2";
+
+function App() {
+  return (
+    <div className="Main-container">
+      <Form1/>
+      <Form2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Forms/src/componets/Form1.jsx
@@ -0,0 +1,321 @@
+import React, { useState } from "react";
+import "./Form.css";
+
+export const Form1 = () => {
+  const [feild, setFeild] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    gender: "",
+    skills: [],
+    date: "",
+    country: "",
+  });
+
+  const [isSubmitted, setSubmitted] = useState(null);
+
+  const [errorfeild, seterrorFeild] = useState({
+    firstname: false,
+    lastname: false,
+    email: false,
+    password: false,
+    gender: false,
+    skills: false,
+    date: false,
+    country: false,
+  });
+
+  const [firstnamelength, setfirstnamelegth] = useState(0);
+
+  const Formvalid = (event) => {
+    const { name, value, type, checked } = event.target;
+    let languages = [...feild.skills];
+    if (name === "firstname") {
+      setfirstnamelegth(value.trim().length);
+      
+    }
+    if (type === "checkbox") {
+      if (checked) {
+        languages.push(value);
+      } else {
+        languages = [...languages.filter((item) => item !== value)];
+      }
+      setFeild((pre) => ({ ...pre, skills: languages }));
+    } else {
+      setFeild((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+
+    let error = false;
+
+    if (name === "firstname" && value === "") {
+      error = true;
+    } else if (name === "lastname" && value === "") {
+      error = true;
+    } else if (
+      name === "email" &&
+      (value === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))
+    ) {
+      error = true;
+    } else if (name === "password" && value === "") {
+      error = true;
+    } else if (name === "gender" && value === "") {
+      error = true;
+    } else if (name === "skills" && languages.length === 0) {
+      error = true;
+    } else if (name === "date" && value === "") {
+      error = true;
+    } else if (name === "country" && value === "") {
+      error = true;
+    }
+    seterrorFeild((prev) => ({
+      ...prev,
+      [name]: error,
+    }));
+  };
+  const handlechange = (event) => {
+    Formvalid(event);
+  };
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    if (isformvalid()) {
+      setSubmitted(true);
+      return;
+    }
+    setSubmitted(false);
+  };
+
+  const isformvalid = () => {
+    const errors = {
+      firstname: false,
+      lastname: false,
+      email: false,
+      password: false,
+      gender: false,
+      skills: false,
+      country: false,
+    };
+    if (feild.firstname === "") {
+      errors.firstname = true;
+    }
+    if (feild.lastname === "") {
+      errors.lastname = true;
+    }
+    if (feild.email === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(feild.email)) {
+      errors.email = true;
+    }
+    if (feild.password === "") {
+      errors.password = true;
+    }
+    if (feild.gender === "") {
+      errors.gender = true;
+    }
+    if (feild.date === "") {
+      errors.date = true;
+    }
+    if (feild.skills.length === 0) {
+      errors.skills = true;
+    }
+    if (feild.country === "") {
+      errors.country = true;
+    }
+    seterrorFeild(errors);
+    if (Object.values(errors).some((error) => error === true)) {
+      return false;
+    }
+    return true;
+  };
+  return (
+    <div className="first-container">
+      <br />
+      <h1>Registration Form</h1>
+
+      <h3 className="h3">Using OnChange</h3>
+      {isSubmitted === true && (
+        <p className="success">Submitted Successfully</p>
+      )}
+      {isSubmitted === false && (
+        <p className="notsuccess">Please fill the form</p>
+      )}
+
+      <form action="#" onSubmit={handlesubmit} autoComplete="off">
+        {/*--------input section----------*/}
+
+        <label htmlFor="" className="typing-label">
+          FirstName
+        </label>
+        <input
+          type="text"
+          name="firstname"
+          className="typing-input"
+          placeholder="FirstName"
+          onChange={handlechange}
+        />
+        {errorfeild.firstname && (
+          <p className="danger">Firstname Is Required</p>
+        )}
+        {firstnamelength >0 && firstnamelength <3 &&(
+          <p className="danger">
+            Firstname must be atleast 2 more characters long
+          </p>
+        )}
+
+        <label htmlFor="" className="typing-label">
+          LastName
+        </label>
+        <input
+          type="text"
+          name="lastname"
+          className="typing-input"
+          placeholder="LastName"
+          onChange={handlechange}
+        />
+        {errorfeild.lastname && <p className="danger">lastname Is Required</p>}
+        <label htmlFor="" className="typing-label">
+          Email
+        </label>
+        <input
+          type="text"
+          name="email"
+          className="typing-input"
+          placeholder="Email"
+          onChange={handlechange}
+        />
+        {errorfeild.email && <p className="danger">Email Is Required</p>}
+        <label htmlFor="" className="typing-label">
+          Password
+        </label>
+        <input
+          name="password"
+          type="password"
+          className="typing-input"
+          placeholder="  password"
+          onChange={handlechange}
+        />
+        {errorfeild.password && <p className="danger">Password Is Required</p>}
+        <br />
+        {/*--------Gender section----------*/}
+
+        <span className="typing-label">Select Your Gender</span>
+        <br />
+        <label htmlFor="male" className="radio">
+          Male
+        </label>
+        <input
+          type="radio"
+          id="male"
+          name="gender"
+          value="male"
+          className="radio"
+          onChange={handlechange}
+        />
+        <label htmlFor="female" className="radio">
+          Female
+        </label>
+        <input
+          type="radio"
+          id="female"
+          name="gender"
+          value="female"
+          className="radio"
+          onChange={handlechange}
+        />
+        <br />
+        {errorfeild.gender && <p className="danger1">Gender Is Required</p>}
+        <br />
+
+        {/*--------Language section----------*/}
+        <span className="checkbox-span">Select Your Languages</span>
+        <br />
+        <label htmlFor="checkbox">Javascript </label>
+        <input
+          type="checkbox"
+          name="skills"
+          value="javascript"
+          className="radio"
+          id="checkbox"
+          onChange={handlechange}
+        />
+        <label htmlFor="checkbox">Css </label>
+        <input
+          type="checkbox"
+          name="skills"
+          id="checkbox"
+          value="css"
+          className="radio"
+          onChange={handlechange}
+        />
+        <label htmlFor="checkbox">Html </label>
+        <input
+          id="checkbox"
+          type="checkbox"
+          name="skills"
+          value="html"
+          className="radio"
+          onChange={handlechange}
+        />
+        <label htmlFor="checkbox" className="php">
+          Php{"  "}
+        </label>
+        <input
+          id="checkbox"
+          type="checkbox"
+          name="skills"
+          value="php"
+          className="radio"
+          onChange={handlechange}
+        />
+        {errorfeild.skills && (
+          <p className="danger1">You Must Be Select Atleast 1 Language</p>
+        )}
+        <br />
+
+        {/*--------Date section----------*/}
+
+        <span htmlFor="date" className="typing-label">
+          Date Of Birth{" "}
+        </span>
+        <input
+          id="date"
+          type="date"
+          name="date"
+          className="date"
+          onChange={handlechange}
+        />
+        {errorfeild.date && <p className="danger1">date Is Required</p>}
+        <br />
+
+        {/*--------country section----------*/}
+
+        <span className="typing-label">Select Your Country</span>
+        <select
+          name="country"
+          id=""
+          className="maincountry"
+          onChange={handlechange}
+        >
+          <option value="">Select</option>
+          <option value="india" className="country">
+            India
+          </option>
+          <option value="uae" className="country">
+            Uae
+          </option>
+          <option value="qatar" className="country">
+            Qatar
+          </option>
+        </select>
+        {errorfeild.country && <p className="danger1">country Is Required</p>}
+        <br />
+        <br />
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
